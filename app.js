3b7b480f322b005e040c747a036d348bfd79c059
Mehul Oswal <mehuloswal21@gmail.com>
2021-06-05 02:01:12 +0530

    Added view engine

--- app.js
+++ app.js
@@ -1,26 +1,33 @@
-const express = require("express");
+const express = require('express');
+
+// express app
 const app = express();
-const port = 3000;
 
-//listen to requests
-app.listen(port, () => {
-  console.log(`Example app listening on port 3000!`);
+// listen for requests
+app.listen(3000);
+
+// register view engine
+app.set('view engine', 'ejs');
+// app.set('views', 'myviews');
+
+app.get('/', (req, res) => {
+  const blogs = [
+    {title: 'Yoshi finds eggs', snippet: 'Lorem ipsum dolor sit amet consectetur'},
+    {title: 'Mario finds stars', snippet: 'Lorem ipsum dolor sit amet consectetur'},
+    {title: 'How to defeat bowser', snippet: 'Lorem ipsum dolor sit amet consectetur'},
+  ];
+  res.render('index', { title: 'Home', blogs });
 });
 
-app.get("/", (req, res) => {
-  res.sendFile("./views/index.html", { root: __dirname });
+app.get('/about', (req, res) => {
+  res.render('about', { title: 'About' });
 });
 
-app.get("/about", (req, res) => {
-  res.sendFile("./views/about.html", { root: __dirname });
+app.get('/blogs/create', (req, res) => {
+  res.render('create', { title: 'Create a new blog' });
 });
 
-//redirects
-app.get("/about-me", (req, res) => {
-  res.redirect("/about");
+// 404 page
+app.use((req, res) => {
+  res.status(404).render('404', { title: '404' });
 });
-
-//defaults-at the bottom only
-app.use((req,res)=>{
-    res.status(404).sendFile("./views/404.html", { root: __dirname });
-})
