27d7e8ddd8d913a2ab2f78e6ef620403a73dd9fa
Mehul Oswal <mehuloswal21@gmail.com>
2021-06-05 11:00:34 +0530

    Rendered ejs

--- app.js
+++ app.js
@@ -4,7 +4,9 @@ const express = require('express');
 const app = express();
 
 // listen for requests
-app.listen(3000);
+app.listen(3000,()=>{
+  console.log("Listening on http://localhost:3000")
+});
 
 // register view engine
 app.set('view engine', 'ejs');
